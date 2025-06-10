4abf4d710d2ae372ead0b4cfea95fe406504482c
Sandeep01042001 <sandeep7668111757@gmail.com>
2025-06-10 10:07:17 +0530

    Complete task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
+     // Task 1: Create a class Product inside Main class
+    static class Product{
 
+        //Task 3: Create a method with the following definition
+         public int product(int x, int y){
+             return x * y;
+         }
+         // Task 4: Create an overloaded method with the following definition
+         public int product(int x, int y, int z){
+             return x * y * z;
+         }
+
+         // Task 5: Create an overloaded method with the following definition
+         public double product(double x, double y){
+             return x * y;
+         }
+     }
+
+    public static void main(String[] args) {
+        // Task 2: Create object of Product in Main function called p
+        Product p = new Product();
+        // Call all overloaded methods
+        System.out.println("Product of 4 and 5: " + p.product(4, 5));
+        System.out.println("Product of 2, 3 and 4: " + p.product(2, 3, 4));
+        System.out.println("Product of 2.5 and 4.2: " + p.product(2.5, 4.2));
+    }
 }
